23b1601474cd8615bedc181274e1d655ce74432f
Septianadaw <septianadaw460@gmail.com>
2025-03-30 19:50:12 +0700

    Add files via upload

---
+++ responsi.sql
@@ -0,0 +1,167 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 30 Mar 2025 pada 14.49
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `responsi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `anggota`
+--
+
+CREATE TABLE `anggota` (
+  `id_anggota` char(4) NOT NULL,
+  `nama_anggota` varchar(50) NOT NULL,
+  `jurusan_anggota` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `anggota`
+--
+
+INSERT INTO `anggota` (`id_anggota`, `nama_anggota`, `jurusan_anggota`) VALUES
+('A001', 'Septiana', 'Informatika'),
+('A002', 'Diyah', 'Akuntansi'),
+('A003', 'Ayu', 'Informatika'),
+('T004', 'Wulandari', 'Manajemen'),
+('T005', 'Cahaya', 'Sistem Informasi');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` char(4) NOT NULL,
+  `judul_buku` varchar(100) NOT NULL,
+  `id_penerbit` char(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `judul_buku`, `id_penerbit`) VALUES
+('B001', 'Basis Data', 'P001'),
+('B002', 'Pengantar Akuntansi', 'P002'),
+('B003', 'Algoritma dan Pemrograman', 'P003'),
+('B004', 'Manajemen Pemasaran', 'P004');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id_transaksi` char(4) NOT NULL,
+  `id_anggota` char(4) DEFAULT NULL,
+  `id_buku` char(4) DEFAULT NULL,
+  `tgl_pinjam` date NOT NULL,
+  `tgl_kembali` date DEFAULT NULL,
+  `denda` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `peminjaman`
+--
+
+INSERT INTO `peminjaman` (`id_transaksi`, `id_anggota`, `id_buku`, `tgl_pinjam`, `tgl_kembali`, `denda`) VALUES
+('T001', 'A001', 'B001', '2025-03-01', '2025-03-08', 0),
+('T002', 'A002', 'B002', '2025-03-02', '2025-03-15', 10000),
+('T003', 'A003', 'B003', '2025-03-05', '2025-03-15', 0),
+('T004', 'T004', 'B004', '2025-03-07', '2025-03-18', 2000),
+('T005', 'T005', 'B001', '2025-03-10', '2025-03-22', 4000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penerbit`
+--
+
+CREATE TABLE `penerbit` (
+  `id_penerbit` char(4) NOT NULL,
+  `nama_penerbit` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `penerbit`
+--
+
+INSERT INTO `penerbit` (`id_penerbit`, `nama_penerbit`) VALUES
+('P001', 'Gramedia'),
+('P002', 'Cemerlang'),
+('P003', 'Salemba'),
+('P004', 'Cemerlang');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `anggota`
+--
+ALTER TABLE `anggota`
+  ADD PRIMARY KEY (`id_anggota`);
+
+--
+-- Indeks untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`),
+  ADD KEY `id_penerbit` (`id_penerbit`);
+
+--
+-- Indeks untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_anggota` (`id_anggota`),
+  ADD KEY `id_buku` (`id_buku`);
+
+--
+-- Indeks untuk tabel `penerbit`
+--
+ALTER TABLE `penerbit`
+  ADD PRIMARY KEY (`id_penerbit`);
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  ADD CONSTRAINT `buku_ibfk_1` FOREIGN KEY (`id_penerbit`) REFERENCES `penerbit` (`id_penerbit`);
+
+--
+-- Ketidakleluasaan untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD CONSTRAINT `peminjaman_ibfk_1` FOREIGN KEY (`id_anggota`) REFERENCES `anggota` (`id_anggota`),
+  ADD CONSTRAINT `peminjaman_ibfk_2` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
