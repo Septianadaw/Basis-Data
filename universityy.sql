83c19f5d6a5cc0c3c389c27fe7c81c9aac4e01ce
Septianadaw <septianadaw460@gmail.com>
2025-06-13 21:10:21 +0700

    Add files via upload

---
+++ universityy.sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 13 Jun 2025 pada 16.09
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `universityy`
+--
+
+DELIMITER $$
+--
+-- Prosedur
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `view_mhs_simple` ()   BEGIN
+    SELECT NPM, nama, no_hp
+    FROM mahasiswa;
+END$$
+
+--
+-- Fungsi
+--
+CREATE DEFINER=`root`@`localhost` FUNCTION `tambah` (`angka1` INT, `angka2` INT) RETURNS INT(11)  BEGIN
+    RETURN angka1 + angka2;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_brg` char(3) NOT NULL,
+  `nama_brg` varchar(100) DEFAULT NULL,
+  `stok` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id_brg`, `nama_brg`, `stok`) VALUES
+('A11', 'Keyboard', 15),
+('A12', 'DVD R-W', 10),
+('A13', 'Modem', 6);
+
+--
+-- Trigger `barang`
+--
+DELIMITER $$
+CREATE TRIGGER `auditBarang` BEFORE INSERT ON `barang` FOR EACH ROW BEGIN
+    IF NOT EXISTS (SELECT id_brg FROM barang WHERE id_brg = NEW.id_brg)
+    THEN
+        SET NEW.nama_brg = NEW.nama_brg;
+        SET NEW.stok = NEW.stok;
+    ELSE
+        SET @status = CONCAT('Id ', NEW.id_brg, ' sudah ada');
+    END IF;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `deleteChild` AFTER DELETE ON `barang` FOR EACH ROW DELETE FROM pembelian
+WHERE id_brg = OLD.id_brg
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `npm` int(11) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `alamat` varchar(100) DEFAULT NULL,
+  `id_kelurahan` int(11) DEFAULT NULL,
+  `jenis_kelamin` char(1) DEFAULT NULL,
+  `no_hp` varchar(20) DEFAULT NULL,
+  `kode_prodi` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`npm`, `nama`, `alamat`, `id_kelurahan`, `jenis_kelamin`, `no_hp`, `kode_prodi`) VALUES
+(12345, 'Imam Adi Nata', 'Kajen', 1, 'L', '081215529989', 1),
+(12346, 'Budi Sugandhi', 'Bekasi', 109, 'L', '085643433321', 2),
+(12347, 'Toha Sitohang', 'Medan', 103, 'L', '08998787876', 2),
+(12348, 'Megawati', 'Condong Catur', 107, 'P', '0839303058', 1),
+(12349, 'PRABOWO SUBIYANTO', 'JKT', 107, 'L', '08132089111', NULL),
+(12780, 'Anis Baswedan', 'Jakarta', 234, 'L', '08989877663', NULL),
+(12786, 'GANJAR PRANOWO', 'JAWA TENGAH', 123, 'L', '087656356622', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembelian`
+--
+
+CREATE TABLE `pembelian` (
+  `id_pem` int(11) NOT NULL,
+  `id_brg` char(3) DEFAULT NULL,
+  `jml_beli` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Trigger `pembelian`
+--
+DELIMITER $$
+CREATE TRIGGER `updateStok` AFTER INSERT ON `pembelian` FOR EACH ROW UPDATE barang
+SET stok = stok + NEW.jml_beli
+WHERE id_brg = NEW.id_brg
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `updateStokEdit` AFTER UPDATE ON `pembelian` FOR EACH ROW UPDATE barang
+SET stok = stok + (NEW.jml_beli - OLD.jml_beli)
+WHERE id_brg = NEW.id_brg
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_brg`);
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`npm`);
+
+--
+-- Indeks untuk tabel `pembelian`
+--
+ALTER TABLE `pembelian`
+  ADD PRIMARY KEY (`id_pem`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
